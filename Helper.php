e9b27f0ca28ae4ed8612388c8806f1abe316b752
Gennadiy Kovalev <gennadiy@weird.company>
2014-11-11 23:22:20 +0600

    notice fix

--- Helper.php
+++ Helper.php
@@ -4,7 +4,7 @@ namespace ComputationCloud;
 
 class Helper
 {
-    static public function is($var, $default = null)
+    static public function is(&$var, $default = null)
     {
         return isset($var) ? $var : $default;
     }
