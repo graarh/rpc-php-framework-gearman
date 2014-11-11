ce4fe0fe335956ba092112b1e29182ffcf005429
Gennadiy Kovalev <gennadiy@weird.company>
2014-11-12 01:33:43 +0600

    redis worker

--- Task/Redis.php
+++ Task/Redis.php
@@ -6,6 +6,21 @@ use ComputationCloud\Helper;
 use ComputationCloud\Exchange\ExchangeInterface;
 use ComputationCloud\Json;
 
+/**
+ * Class Redis
+ *
+ * Redis tasks has no "crashed" tasks protection
+ *
+ * Config example
+ * [
+ *  'connection' => ['server' => 'localhost'],
+ *  'function' => 'sum',
+ *  'exchange' => new ComputationCloud\Exchange\Redis(...)
+ *  ],
+ * ]
+ *
+ * @package ComputationCloud\Task
+ */
 abstract class Redis implements TaskInterface
 {
     use Json;
@@ -17,24 +32,31 @@ abstract class Redis implements TaskInterface
 
     private $key;
     private $name;
+    private $result = null;
 
-    public function __construct($config, $worker = false)
+    private function getCreatedTasksSet()
     {
-        if (!$worker) {
-            $server = Helper::is($config['redis']['server'], ['host' => 'localhost']);
-            $this->redisInstance = new \Redis();
-            $this->redisInstance->connect(
-                Helper::is($server['host'], 'localhost'),
-                Helper::is($server['port'], 6379),
-                Helper::is($server['timeout'], 0.0)
-            );
+        return $this->name . '-created';
+    }
 
-            $this->name = Helper::is($config['redis']['function'], get_class($this));
+    public function __construct(Array $config, $worker = false)
+    {
+        $server = Helper::is($config['connection']['server'], ['host' => 'localhost']);
+        $this->redisInstance = new \Redis();
+        $this->redisInstance->connect(
+            Helper::is($server['host'], 'localhost'),
+            Helper::is($server['port'], 6379),
+            Helper::is($server['timeout'], 0.0)
+        );
 
+        $this->name = Helper::is($config['function'], get_class($this));
+
+        if ($worker === false) {
             do {
-                $this->key = $this->name.'-'.uniqid('', true);
+                $this->key = $this->name . '-' . uniqid('', true);
             } while ($this->redisInstance->exists($this->key));
         }
+
         if (!($this->exchange = Helper::is($config['exchange']))) {
             throw new Exception("Exchange not defined", 1);
         }
@@ -50,39 +72,49 @@ abstract class Redis implements TaskInterface
         if (!$this->isComplete()) {
             throw new Exception("Do not run another task on the same instance until current is working", 1);
         }
+        $this->result = null;
 
         $data = [
             'params' => $params,
-            'status' => 'created',
             'time' => time(),
         ];
 
         if (!$this->redisInstance->set($this->key, $this->encode($data))) {
             throw new Exception("Cannot set key '{$this->key}' to redis");
         }
-        if (!$this->redisInstance->sAdd($this->name, $this->key)) {
+        if (!$this->redisInstance->sAdd($this->getCreatedTasksSet(), $this->key)) {
             throw new Exception("Task was not added to tasks set `$this->key` to redis");
         }
     }
 
     public function isComplete()
     {
-        if (!$this->redisInstance->exists($this->key)) {
-            return true;
-        }
-        $status = Helper::is($this->decode($this->redisInstance->get($this->key)), 'status');
-        if ($status !== 'complete') {
-            return false;
-        }
-        return true;
+        return !$this->redisInstance->exists($this->key);
     }
 
     public function result()
     {
+        if (!is_null($this->result)) {
+            return $this->result;
+        }
         if (!$this->isComplete()) {
             return false;
         }
-        return $this->exchange->get();
+        $this->result = $this->exchange->pop();
+        $this->redisInstance->delete($this->key);
+        return $this->result;
+    }
+
+    public function work()
+    {
+        $jobName = $this->redisInstance->sPop($this->getCreatedTasksSet());
+        if (!$this->redisInstance->exists($jobName)) {
+            throw new Exception("Job not found, but present in queue", 1);
+        }
+        $data = $this->decode($this->redisInstance->get($jobName));
+        $result = $this->worker($data['params']);
+        $this->exchange->put($result);
+        $this->redisInstance->delete($jobName);
     }
 
     abstract public function worker($params);
--- Task/TaskInterface.php
+++ Task/TaskInterface.php
@@ -4,12 +4,47 @@ namespace ComputationCloud\Task;
 
 interface TaskInterface
 {
-    public function __construct($config);
+    /**
+     * @param array $config
+     * @param bool $worker
+     */
+    public function __construct(Array $config, $worker = false);
 
+    /**
+     * Get instance of manager, that this tasks using
+     * E.g. redis or gearman
+     * @return mixed
+     */
     public function getCoreInterface();
+
+    /**
+     * Worker implementation
+     * @param array $params
+     * @return array
+     */
     public function worker($params);
 
+    /**
+     * Run instance of worker with given parameters
+     * @param array $params
+     * @return array
+     */
     public function run($params);
+
+    /**
+     * Is worker finished?
+     * @return bool
+     */
     public function isComplete();
+
+    /**
+     * Get worker result
+     * @return array
+     */
     public function result();
+
+    /**
+     * Run worker instance
+     */
+    public function work();
 }
