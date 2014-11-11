5dfcb614c950180aa981ae1ffaa70c5de97e7031
Gennadiy Kovalev <gennadiy@weird.company>
2014-11-11 22:16:57 +0600

    renames

--- Tests/Exchange/FileExchangeTestCase.php
+++ Tests/Exchange/FileExchangeTestCase.php
@@ -9,14 +9,14 @@ class FileExchangeTestCase extends PHPUnit_Framework_TestCase
         $data = $exchange->get();
         $this->assertEquals("test", $data, "'test' string was put into exchange");
 
-        $this->setExpectedExceptComputationCloudnager\\Exchange\\Exception");
+        $this->setExpectedException("ComputationCloud\\Exchange\\Exception");
         $exchange->get();
     }
 
     public function testEmptyGet()
     {
-        $exchaComputationCloudaskManager\Exchange\File("/tmp");
-        $this->setExpeComputationCloudon("TaskManager\\Exchange\\Exception");
+        $exchange = ComputationCloud\Exchange\File("/tmp");
+        $this->setExpectedException("ComputationCloud\\Exchange\\Exception");
         $exchange->get();
     }
 }
