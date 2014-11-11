b83823a9f40f063469d77f8181a37e427a32273a
Gennadiy Kovalev <gennadiy@weird.company>
2014-11-11 23:59:01 +0600

    redis exchange

---
+++ Exchange/Redis.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ComputationCloud\Exchange;
+
+class Redis implements ExchangeInterface
+{
+    use Json;
+
+    /** @var \Redis $redis */
+    private $redis;
+    private $key;
+    public function __construct(\Redis $instance)
+    {
+        $this->redis = $instance;
+        do {
+            $this->key = uniqid("", true);
+
+        } while ($this->redis->exists($this->key));
+    }
+
+    public function put($data)
+    {
+        if (!$this->redis->set($this->key, $this->encode($data))) {
+            throw new Exception("Cannot set key `{$this->key}` to redis");
+        }
+    }
+
+    public function get()
+    {
+        if (!$this->redis->exists($this->key)) {
+            throw new Exception("Key `{$this->key}` not found in redis storage", 1);
+        }
+        return $this->decode($this->redis->get($this->key));
+
+    }
+}
